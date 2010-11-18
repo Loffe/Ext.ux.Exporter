120cdfc5f85d20e0dcd751c5d110b30ce736a21c
Erik Eloff <erik@eloff.se>
2010-11-18 23:30:54 +0100

    Check for hidden column, strip html tags and escape quotes

--- CSVFormatter/CSVFormatter.js
+++ CSVFormatter/CSVFormatter.js
@@ -18,7 +18,11 @@ Ext.ux.Exporter.CSVFormatter = Ext.extend(Ext.ux.Exporter.Formatter, {
     var cols = [];
     Ext.each(colModel.config, function(column) {
       // todo: check hidden props
-      cols.push(column.header);
+      if (!column.hidden) {
+        var stripped = this.stripTags(column.header);
+        var escapedText = this.escapeTextSeperator(stripped);
+        cols.push(escapedText);
+      }
     }, this);
     return cols.join(",");
   },
@@ -33,8 +37,39 @@ Ext.ux.Exporter.CSVFormatter = Ext.extend(Ext.ux.Exporter.Formatter, {
     var cols = [];
     Ext.each(colModel.config, function(column) {
       // todo: check hidden props
-      cols.push(row.data[column.dataIndex]);
-    });
+      if (!column.hidden) {
+        var stripped = this.stripTags((row.data[column.dataIndex]));
+        var escapedText = this.escapeTextSeperator(stripped);
+        cols.push(escapedText);
+      }
+    }, this);
     return cols.join(",");
+  },
+  /**
+   * Little helper function to strip tags from a string.
+   * @param strMod
+   * @return strMod
+   */
+  stripTags: function(strMod){
+    if (typeof(strMod) === "string") {
+      strMod = strMod.replace(/<(.|\n)*?>/gi, '');
+    }
+    var	tarea = document.createElement('textarea');
+    tarea.innerHTML = strMod;
+    return tarea.value;
+  },
+
+  /**
+   * Little helper function to escape CSV Text Seperator.
+   * @param strMod
+   * @return strMod
+   */
+  escapeTextSeperator: function(strMod){
+    if (typeof(strMod) === "string") {
+      strMod = strMod.replace(/"/gi, '""');
+    }
+    var	tarea = document.createElement('textarea');
+    tarea.innerHTML = strMod;
+    return '"' + tarea.value + '"';
   }
 });
